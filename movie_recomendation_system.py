eff86d12fab44330ffa4510563cfa47a8760fdae
Suchetan Bir <suchetanbir@lm-1c57dc51a341.dhcp.usask.ca>
2025-02-28 14:45:49 -0600

    added a convert function that converts the string list into a normal list so tha it can be looped over.

--- movie_recomendation_system.py
+++ movie_recomendation_system.py
@@ -1,5 +1,6 @@
 import numpy as np
 import pandas as pd
+import ast 
 
 # Importing movies and credit file
 movies = pd.read_csv('tmdb_5000_movies.csv')
@@ -22,6 +23,25 @@ movies = movies[['movie_id','title','overview','genres','keywords','cast']]
 # Data pre-processing   
     # 1. removing missing data
     # 2. removing duplicate data
-    # 3. joining genres, keywords, cast to creates tags 
-movies.dropna(inplace=True)
-print(movies.isnull().sum())
+    # 3. joining genres, keywords, cast to creates tags
+
+movies.dropna(inplace=True) # dropna removes rows with atleast one NaN and inplace modifies the movies instead of giving a new one
+
+# Now to just get the tags instead of a list 
+# for example in genre
+# instead of {[id = , name = ], [id = , name = ]} we want [name, name, name] i.e. [adventure, fantasy, scifi]
+
+# the function wont work as the list is a string 
+    # need to use ast module
+        # will convert the string into a list 
+        
+def convert(obj):
+    list_names = []
+    for i in ast.literal_eval(obj):
+        list_names.append(i['name'])
+    return list_names
+
+#print(movies['genres'].apply(convert))
+
+
+
